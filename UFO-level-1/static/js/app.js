48d2bd6d6d04abe9c7c2aefcda242b00123d854b
Rachel Chan <chan.rce@gmail.com>
2020-12-15 13:49:59 -0500

    added table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,7 +3,26 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
+//Refererence to table body
+var tbody = d3.select("tbody");
+
+//Console.log the UFO data from data.js
+data.forEach(function(UFOReport) {
+    console.log(UFOReport);
+    var row = tbody.append("tr");
+
+    Object.entries(UFOReport).forEach(function([key,value]) {
+        console.log(key,value);
+    });
+    
+});
+
+
+
+
+
 //Step 1: Create a Build Table function
+
 
 //Step 2: Create a handle Click function
 
@@ -18,3 +37,10 @@ var tableData = data;
     //1. Make sure the table generates on Page Load
     //2. Be able to filter multiple lines, and is recreated with only newly selected data
     //3. If you clear out filter, the entire table should load
+
+
+
+
+
+
+    

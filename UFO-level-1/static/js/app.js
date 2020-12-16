c5ffd92d45e6038fea01097d362efacc5375c44f
Rachel Chan <chan.rce@gmail.com>
2020-12-15 23:47:43 -0500

    added filtered data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,6 +7,7 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 //Console.log the UFO data from data.js
+//Adding table to website
 data.forEach(function(UFOReport) {
     console.log(UFOReport);
     var row = tbody.append("tr");
@@ -22,8 +23,36 @@ data.forEach(function(UFOReport) {
 });
 
 
+//Form and button data
 
+//Select button and form
+var button = d3.select("#filter-btn");
+var form = d3.select("#form-control");
 
+//Event handlers
+button.on("click", runEnter);
+form.on("click", runEnter)
+
+//Complete the event handler function for the form
+function runEnter() {
+    //Prevent page from refreshing
+    d3.event.preventDefault();
+    //Select input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    //Get value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    //Use form input to filter data by date
+    var filterData = tableData.filter(function(sighting){
+        return sighting.datetime === inputValue;
+
+   
+    })
+
+    console.log(filterData);
+
+};
 
 //Step 1: Create a Build Table function
 

4316a746df6b7853649df3cc042e20f226780725
Rachel Chan <chan.rce@gmail.com>
2020-12-18 22:13:21 -0500

    cleaned app file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -70,20 +70,3 @@ function runEnter() {
         
     });
 };
-
-//Step 1: Create a Build Table function
-
-
-//Step 2: Create a handle Click function
-
-    //You'll need to filter the data according to the form
-
-//Step 2: Event Listener
-
-//Step 1: Build table when page loads
-
-
-//Hints:
-    //1. Make sure the table generates on Page Load
-    //2. Be able to filter multiple lines, and is recreated with only newly selected data
-    //3. If you clear out filter, the entire table should load
